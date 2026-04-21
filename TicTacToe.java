95c7208c9463ab83fdeea6426c0e80836c81cd66
Anushka Dutta <dutta.anushka7002@gmail.com>
2026-04-21 13:33:30 +0530

    UC5 : Validate user move with boundary and empty cell checks

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,22 @@
-/**
- * TicTacToe
- * UC1 initializes and displays an empty Tic-Tac-Toe board in a proper
- * grid format. This use case introduces 2D arrays, nested loops,
- * and formatted console output.
- */
+import java.util.Random;
+import java.util.Scanner;
 public class TicTacToe {
     static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
     public static void main(String[] args) {
         initializeBoard();
         printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
+        int slot = getUserSlot();
+        int row = getRowFromSlot(slot);
+        int col = getColFromSlot(slot);
+        System.out.println("Slot entered: " + slot);
+        System.out.println("Row: " + row);
+        System.out.println("Column: " + col);
+        System.out.println("Move valid: " + isValidMove(row, col));
     }
     static void initializeBoard() {
         for (int row = 0; row < 3; row++) {
@@ -30,5 +38,48 @@ public class TicTacToe {
             System.out.println();
             System.out.println("-------------");
         }
+    }
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2);   // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+    static void displayTossResult() {
+        System.out.println();
+        if (isHumanTurn) {
+            System.out.println("Human starts first with symbol " + humanSymbol);
+            System.out.println("Computer gets symbol " + computerSymbol);
+        } else {
+            System.out.println("Computer starts first with symbol " + computerSymbol);
+            System.out.println("Human gets symbol " + humanSymbol);
+        }
+    }
+    static int getUserSlot() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter slot (1-9): ");
+            int slot = sc.nextInt();
+
+            return slot;
+        }
+    }
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 &&
+               col >= 0 && col < 3 &&
+               board[row][col] == '-';
     }
 }
